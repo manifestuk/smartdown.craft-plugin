45ebe889ff73fd8f8a72fadbeef11f565b1c0793
Stephen Lewis <monooso.github.com@experiencehq.net>
2015-07-30 00:19:16 +0100

    Implement parser hooks.
    - modifySmartDownMarkupInput
    - modifySmartDownMarkupOutput
    - modifySmartDownTypographyInput
    - modifySmartDownTypographyOutput

--- src/smartdown/utilities/Parser.php
+++ src/smartdown/utilities/Parser.php
@@ -29,18 +29,6 @@ class Parser
     }
 
     /**
-     * Runs the given string through the "markup" parser (MarkdownExtra).
-     *
-     * @param string $source
-     *
-     * @return string
-     */
-    public function parseMarkup($source)
-    {
-        return MarkdownExtra::defaultTransform($source);
-    }
-
-    /**
      * Runs the given string through "typography" parser (SmartyPants).
      *
      * @param string $source
@@ -49,6 +37,56 @@ class Parser
      */
     public function parseTypography($source)
     {
-        return SmartyPants::defaultTransform($source);
+        $source = $this->callHook('modifySmartDownTypographyInput', $source);
+        $source = SmartyPants::defaultTransform($source);
+        $source = $this->callHook('modifySmartDownTypographyOutput', $source);
+
+        return $source;
+    }
+
+    /**
+     * Runs the given string through the "markup" parser (MarkdownExtra).
+     *
+     * @param string $source
+     *
+     * @return string
+     */
+    public function parseMarkup($source)
+    {
+        $source = $this->callHook('modifySmartDownMarkupInput', $source);
+        $source = MarkdownExtra::defaultTransform($source);
+        $source = $this->callHook('modifySmartDownMarkupOutput', $source);
+
+        return $source;
+    }
+
+    /**
+     * Calls the specified hook, with the specified "source" text. Differs to
+     * the standard Craft PluginsService::call method, in that the results are
+     * cumulative. That is, FirstPlugin receives the source string,
+     * SecondPlugin receives the source string after it has been parsed by
+     * FirstPlugin, and so forth.
+     *
+     * There are clearly some potential downsides to this, as hook handlers
+     * could find themselves competing to parse the string. However, that would
+     * still be the case with the standard PluginsService::call method, it's
+     * just the results would be a lot more difficult to process.
+     *
+     * @param $hook
+     * @param $source
+     *
+     * @return mixed
+     */
+    private function callHook($hook, $source)
+    {
+        $result = $source;
+
+        foreach (\Craft\craft()->plugins->getPlugins() as $plugin) {
+            if (method_exists($plugin, $hook)) {
+                $result = $plugin->$hook($source);
+            }
+        }
+
+        return $result;
     }
 }
